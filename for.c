48ea42806619045b3c18dd49318e5d9b3cb41647
Galvin <galvin@GalvindeMacBook-Air.local>
2016-04-10 20:57:23 +0800

    update for.c to test one char result.

--- for.c
+++ for.c
@@ -3,9 +3,14 @@
 
 int main(){
 	int n=0;
+	int b=20;
+	char A[11]={};
 	for(n=0; n<10; n++){
+		A[n]='A';
 		printf("%d\n",n);
+		printf("A: %s\n", A);
 	}	
-
+	b-=n;
+	printf("A:%s, n=%d b=%d\n", A, n, b);
 	return 0;
 }
