031b1b0dd92f9c2d89958402ec19a2fdd0bdd4e1
Galvin <galvin@galvindeair.home>
2016-04-28 01:00:41 +0800

    add insert linked function in linked_lisk

--- linked_list.c
+++ linked_list.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-
 typedef struct Node{
 	int date;
 	struct Node *next;
@@ -22,18 +21,45 @@ node* createNode(int date, node* pNode){
 	return p;
 }
 
+void insertNode( node* aNode, node* bNode, node* newNode){
+	
+	if(aNode)
+		aNode->next=newNode;
+
+	while(newNode->next != NULL){
+		newNode=newNode->next;
+	//	printf("newNode date:%d\n", newNode->date);
+	}
+
+	if(newNode->next == NULL){
+		newNode->next=bNode;
+	}else{
+		printf("fail to insertNode\n");
+	}	
+
+}
+
 void showNode(const node* p){
 	printf("%d ", p->date);
 }
 
 int main(){
 	node *pHead=NULL;
+	node *pHead2=NULL;
 	node *p=NULL;
 	int i=0;
 
-	for(i=4;i>0;i--){
+	/*Not only number. Add date list you want.*/
+	for(i=4;i>0;i--){	
 	pHead=createNode(i,pHead);
 	}
+	
+	for(i=10;i>5;i--){
+	pHead2=createNode(i,pHead2);
+	}
+
+	p=pHead->next;
+	insertNode(p, p->next, pHead2);
 
 	/*
 	pHead=createNode(4, pHead);
@@ -52,8 +78,11 @@ int main(){
 		p=p->next;
 	}
 	printf("\n");
+	printf("pHead offest date be: %d\n", pHead->next->next->next->date);
 	
 	p=pHead;
+
+	/*delete*/
 	while(p != NULL){
 		node *pTmp = p;
 		p = p->next;
