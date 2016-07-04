f0536375087f700b75d81a42eae6fa660bb86f02
Galvin <galvin@galvindeair.home>
2016-07-04 16:29:20 +0800

    Add addNode function in linked list, also implement debug print macros in linked list.

--- linked_list.c
+++ linked_list.c
@@ -2,14 +2,20 @@
 #include <stdlib.h>
 #include <string.h>
 
+//#define DEBUG
+#ifdef DEBUG
+#define DEBUG_PRINT(fmt, args...)    printf(fmt, ##args)
+#define SHOW_NOTE(x)	showNode(x)
+#else
+#define DEBUG_PRINT(fmt, args...)    /* Don't do anything in release builds */
+#define SHOW_NOTE(x)
+#endif
 typedef struct Node{
 	int date;
 	struct Node *next;
 }node;
 
 node* createNode(int date, node* pNode){
-	//node x = {date, pNode};
-	printf("date:%d \n", date);
 	node *p = (node *)malloc(sizeof(node));
 
 	if(p == NULL){
@@ -21,6 +27,26 @@ node* createNode(int date, node* pNode){
 	return p;
 }
 
+void showNode(const node* p){
+	while(p != NULL){
+	    printf("%d ", p->date);
+	    p=p->next;
+	}
+	printf("\n");
+}
+
+void addNode( node* pre_Node, node* new_Node){
+	node *p=NULL;
+	p=pre_Node;
+
+	while(p->next != NULL){
+		p=p->next;
+	}	
+
+	if(p->next == NULL)
+	    p->next=new_Node;
+}
+
 void insertNode( node* aNode, node* bNode, node* newNode){
 	
 	if(aNode)
@@ -28,7 +54,7 @@ void insertNode( node* aNode, node* bNode, node* newNode){
 
 	while(newNode->next != NULL){
 		newNode=newNode->next;
-	//	printf("newNode date:%d\n", newNode->date);
+		DEBUG_PRINT("newNode date:%d\n", newNode->date);
 	}
 
 	if(newNode->next == NULL){
@@ -39,46 +65,51 @@ void insertNode( node* aNode, node* bNode, node* newNode){
 
 }
 
-void showNode(const node* p){
-	printf("%d ", p->date);
-}
-
 int main(){
 	node *pHead=NULL;
 	node *pHead2=NULL;
+	node *pHead3=NULL;
 	node *p=NULL;
 	int i=0;
 
 	/*Not only number. Add date list you want.*/
-	for(i=4;i>0;i--){	
-	pHead=createNode(i,pHead);
-	}
+	for(i=5;i>0;i--)	
+	    pHead=createNode(i,pHead);
+	DEBUG_PRINT("pHead: ");SHOW_NOTE(pHead);
 	
-	for(i=10;i>5;i--){
-	pHead2=createNode(i,pHead2);
-	}
+	if(pHead == NULL)
+	    printf("pHead linked fail\n");
 
-	p=pHead->next;
-	insertNode(p, p->next, pHead2);
+	for(i=10;i>5;i--)
+	    pHead2=createNode(i,pHead2);
+	DEBUG_PRINT("pHead2: ");SHOW_NOTE(pHead2);
+	
+	if(pHead2 == NULL)
+	    printf("pHead2 linked fail\n");
 
-	/*
-	pHead=createNode(4, pHead);
-	printf("pHead->Date: %d\n", pHead->date);
-	pHead=(node *)createNode(3, pHead);
-	printf("pHead->Date: %d\n", pHead->date);
-	*/
+/*Add Node*/
+	addNode(pHead, pHead2);
+
+	printf("Node1 - Node2 - End\n");
+	showNode(pHead);
+
+	for(i=15;i>10;i--)
+	    pHead3=createNode(i,pHead3);
+	DEBUG_PRINT("pHead3: ");SHOW_NOTE(pHead3);
+	
+	if(pHead3 == NULL)
+	    printf("linked fail\n");
+
+/*Insert Node*/
+	insertNode(pHead->next, pHead->next->next, pHead3);
 
 	if(pHead == NULL)
-		printf("linked fail\n");
+	    printf("linked fail\n");
+
+	printf("pre_Node1--Node3--end_Node1 - Node2 - End \n");	
+	showNode(pHead);
 	
-	p=pHead;
-	printf("start linked list\n");	
-	while(p != NULL){
-		showNode(p);
-		p=p->next;
-	}
-	printf("\n");
-	printf("pHead offest date be: %d\n", pHead->next->next->next->date);
+	DEBUG_PRINT("pHead offest date be: %d\n", pHead->next->next->next->date);
 	
 	p=pHead;
 
@@ -89,5 +120,4 @@ int main(){
 		free(pTmp);
 	}
 
-	
 }
