29ffcb9ca8c87ff2ea3dd8e95327f758d12962c8
Galvin Li <galvin.ch.li@mail.foxconn.com>
2017-01-16 15:13:56 +0800

    update float.c
    Signed-off-by: Galvin Li <galvin.ch.li@mail.foxconn.com>

--- float.c
+++ float.c
@@ -6,6 +6,8 @@
 int main(){
 	float a = 1.0f;
 	double b = 1.0; /*default 常數 is double*/
+	printf("float size is %d\n", sizeof(float));
+	printf("dauble size is %d\n", sizeof(double));
 	printf("%0.20f\n", a);	/*%f 8 byte */
 	printf("%0.20f\n", b);	/*%f 8 byte */
 
